ce0982822d7b6cacef40d72d20e2f5fd209d3c6d
elhunter97 <hanbaongoc14091271@gmail.com>
2024-04-15 09:44:49 +0700

    Update

--- bank_interest_rate/bank.js
+++ bank_interest_rate/bank.js
@@ -1,5 +1,9 @@
 //Số tiền lãi theo tháng = Số tiền gửi x lãi suất (%năm)/12 x số tháng gửi
 let month_send = +prompt('Input month you send');
-let rate = +prompt('Input rate');
+let rate = +prompt('Input rate'); // lãi hàng tháng
+let rateMonth = rate/100;
 let money = +prompt('Input money you send');
-
+for (let i = 1; i < month_send; i++) {
+    money+=rate;
+}
+document.write('Money you must pay for bank = '+money);
